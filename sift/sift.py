fe1db32c0eef399872cbb2ba9ab7bf21381ec473
Joscha <joscha@plugh.de>
2022-09-30 00:09:49 +0200

    Iterate through pages in dump

--- sift/sift.py
+++ sift/sift.py
@@ -1,2 +1,21 @@
+import sys
+from pathlib import Path
+
+import mwxml  # https://pythonhosted.org/mwxml/
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def process_page(page):
+    # https://pythonhosted.org/mwxml/iteration.html#mwxml.Page
+    eprint(f"{page.id:8} - {page.title}")
+    if len(list(page)) != 1:
+        eprint(f"{page.id:8} - {page.title} - {len(list(page))}")
+
+
 def main():
-    print("Hello world")
+    dump = mwxml.Dump.from_file(sys.stdin)
+    for page in dump.pages:
+        process_page(page)
