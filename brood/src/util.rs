a9435e4f64c900bb33e9f84eba0e10fd7e64ff79
Joscha <joscha@plugh.de>
2022-10-22 00:01:04 +0200

    Lowercase only first char when normalizing

--- brood/src/util.rs
+++ brood/src/util.rs
@@ -1,3 +1,11 @@
 pub fn normalize_link(link: &str) -> String {
-    link.trim().to_lowercase().replace(' ', "_")
+    let link = link.trim().replace(' ', "_");
+
+    // Make only first char lowercase
+    link.chars()
+        .next()
+        .iter()
+        .flat_map(|c| c.to_lowercase())
+        .chain(link.chars().skip(1))
+        .collect::<String>()
 }
